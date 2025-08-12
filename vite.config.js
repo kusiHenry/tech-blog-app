799c8264f76568c451ba52d311908ddb22001f18
isaac <lilykusi84@gmail.com>
2025-08-12 16:19:04 +0100

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tech-blog-app/',
   build: {
     outDir: 'build',
-  },
-  server: {
-    port: 3000,
   },
   plugins: [react()],
 })
